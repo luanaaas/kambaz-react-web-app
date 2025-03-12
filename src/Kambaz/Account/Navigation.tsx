a58c4e39fffbbee4c2244ee35ae0b96b79ea0a92
luanaaas <97379025+luanaaas@users.noreply.github.com>
2025-03-12 10:32:17 -0400

    a4 minor edits2

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -1,25 +1,41 @@
-import { ListGroup } from "react-bootstrap";
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 import { useSelector } from "react-redux";
 
 
 export default function AccountNavigation() {
     const { currentUser } = useSelector((state: any) => state.accountReducer);
-    const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+    const { pathname } = useLocation();
+    const links = currentUser
+        ? [{ label: "Profile", path: "/Kambaz/Account/Profile" }]
+        : [
+            { label: "Sign in", path: "/Kambaz/Account/Signin" },
+            { label: "Sign up", path: "/Kambaz/Account/Signup" },
+        ];
     return (
-        <ListGroup id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
-            {!currentUser && (<>
-            <ListGroup.Item as={Link} to="/Kambaz/Account/Signin"
-                className="list-group-item active border border-0" >Signin</ListGroup.Item>
-            <br />
-            <ListGroup.Item as={Link} to="/Kambaz/Account/Signup"
-                className="list-group-item text-danger border border-0">Signup</ListGroup.Item>
-            <br />
-            </>)}
-            {currentUser && <ListGroup.Item as={Link} to="/Kambaz/Account/Profile"
-                className="list-group-item text-danger border border-0">Profile</ListGroup.Item>}
-        </ListGroup>
+        <div id="wd-account-navigation">
+            {links.map((link) => (
+                <Link to={link.path}
+                key={link.label}
+                    id={`wd-account-${link.label}`}
+                    className={`fs-5 d-block mb-2 ps-1 me-3 ${pathname.includes(link.label) ? "active wd-black-text" : "text-danger"}`}> {link.label} </Link>
+            ))}
+        </div>
     );
+}
+//     return (
+//         <ListGroup id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
+//             {!currentUser && (<>
+//             <ListGroup.Item as={Link} to="/Kambaz/Account/Signin"
+//                 className="list-group-item active border border-0" >Signin</ListGroup.Item>
+//             <br />
+//             <ListGroup.Item as={Link} to="/Kambaz/Account/Signup"
+//                 className="list-group-item text-danger border border-0">Signup</ListGroup.Item>
+//             <br />
+//             </>)}
+//             {currentUser && <ListGroup.Item as={Link} to="/Kambaz/Account/Profile"
+//                 className="list-group-item text-danger border border-0">Profile</ListGroup.Item>}
+//         </ListGroup>
+//     );
 
     
-}
+// }
